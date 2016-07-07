7d96cbebe5fc778fc88c0a3bea82551a87eff18e
Alex Tong <alexdthi@gmail.com>
2016-07-07 13:48:15 -1000

    FINISHED PROJ
    HOLY LOVE GOD

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
 var monHealth =100;
 var hitpower;
 var hit = function() {
-	hitPower = Math.floor(Math.random()*11)
+	hitpower = Math.floor(Math.random()*11);
+	monHealth -= hitpower;
 
-monHealth-=hitPower
+	if (monHealth <= 0) {
+
+		monHealth = 0;
+		document.getElementById('mondead').innerHTML = "Your monster is dead!";
+		console.log(monHealth);
+
+		document.getElementById('monalive').innerHTML = "Your monster is at "  + monHealth;
+	} else {
+
+		document.getElementById('monalive').innerHTML = "Your monster is at "  + monHealth;
+
+
+	}
 }
+
